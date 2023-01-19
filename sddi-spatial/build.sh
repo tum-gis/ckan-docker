fe7208206f61ea0105b22430f7d2981a1311e26c
Bruno Willenborg <b.willenborg@tum.de>
2023-01-19 11:27:52 +0100

    add build push helper scripts

---
+++ sddi-spatial/build.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+REPO="reg.gitlab.brunowillenborg.de/ckan/sddi-spatial"
+CKAN_VERSION=${1:-2.9.5}
+
+# Build with verbose output
+docker build . \
+  --progress=plain \
+  --build-arg CKAN_VERSION="$CKAN_VERSION" \
+  -t "$REPO:$CKAN_VERSION"
