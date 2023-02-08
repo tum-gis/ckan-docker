dd043571b634e881689f0ad8d8dc86013016a3f6
Bruno Willenborg <b.willenborg@tum.de>
2023-02-08 17:25:06 +0100

    drop ckanapi tool

--- sddi-base/afterinit.d/01_create_groups.sh
+++ sddi-base/afterinit.d/01_create_groups.sh
@@ -2,5 +2,5 @@
 
 echo "Create SDDI categories, topics, groups, and organizations ..."
 envsubst < "$APP_DIR/docker-afterinit.d/groups.jsonl" > "$APP_DIR/docker-afterinit.d/groups_subst.jsonl"
-ckanapi batch -c "${APP_DIR}/production.ini" -I "$APP_DIR/docker-afterinit.d/groups_subst.jsonl"
+ckan -c "${APP_DIR}/production.ini" api batch -c "${APP_DIR}/production.ini" -I "$APP_DIR/docker-afterinit.d/groups_subst.jsonl"
 echo "Create SDDI categories, topics, groups, and organizations ...done!"
