04ecd53d4047162c47f3eaa194c893f4b2f85bab
Bruno Willenborg <b.willenborg@tum.de>
2023-02-28 14:32:19 +0100

    update Dockerfile for new ckanext-grouphierarchy

--- sddi-base/afterinit.d/01_create_groups.sh
+++ sddi-base/afterinit.d/01_create_groups.sh
@@ -1,6 +1,5 @@
 #!/usr/bin/env bash
 
 echo "Create SDDI categories, topics, groups, and organizations ..."
-envsubst < "$APP_DIR/docker-afterinit.d/groups.jsonl" > "$APP_DIR/docker-afterinit.d/groups_subst.jsonl"
-ckanapi batch -c "${APP_DIR}/production.ini" -I "$APP_DIR/docker-afterinit.d/groups_subst.jsonl"
+ckan -c "${APP_DIR}/production.ini" grouphierarchy init_data
 echo "Create SDDI categories, topics, groups, and organizations ...done!"
