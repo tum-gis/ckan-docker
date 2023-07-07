78e46dcd3083add829602bbe112e5a46e9df4c51
Bruno Willenborg <b.willenborg@tum.de>
2023-07-07 16:20:40 +0200

    Update local build scripts

--- build-push.sh
+++ build-push.sh
@@ -10,16 +10,17 @@ cd sddi-base
 
 # sddi-base:TAG
 docker build . \
-  --no-cache \
   -t "$IMAGE_REPOSITORY/ckan-sddi-base:${IMAGE_TAG}"
+
+docker push "$IMAGE_REPOSITORY/ckan-sddi-base:${IMAGE_TAG}"
 
 # sddi-base:TAG-debug
 docker build . -f Dockerfile.debug \
-  --no-cache \
   --build-arg "BASEIMAGE_REPOSITORY=$IMAGE_REPOSITORY/ckan-sddi-base" \
   --build-arg BASEIMAGE_VERSION=${IMAGE_TAG} \
   -t "$IMAGE_REPOSITORY/ckan-sddi-base:${IMAGE_TAG}-debug"
 
+docker push "$IMAGE_REPOSITORY/ckan-sddi-base:${IMAGE_TAG}-debug"
 printf "\n\n"
 
 # sddi ########################################################################
@@ -27,18 +28,19 @@ cd ../sddi
 
 # sddi:TAG
 docker build . \
-  --no-cache \
   --build-arg "BASEIMAGE_REPOSITORY=$IMAGE_REPOSITORY/ckan-sddi-base" \
   --build-arg BASEIMAGE_VERSION=${IMAGE_TAG} \
   -t "$IMAGE_REPOSITORY/ckan-sddi:${IMAGE_TAG}"
 
+docker push "$IMAGE_REPOSITORY/ckan-sddi:${IMAGE_TAG}"
+
 # sddi:TAG-debug
 docker build . \
-  --no-cache \
   --build-arg "BASEIMAGE_REPOSITORY=$IMAGE_REPOSITORY/ckan-sddi-base" \
   --build-arg BASEIMAGE_VERSION=${IMAGE_TAG}-debug \
   -t "$IMAGE_REPOSITORY/ckan-sddi:${IMAGE_TAG}-debug"
 
+docker push "$IMAGE_REPOSITORY/ckan-sddi:${IMAGE_TAG}-debug"
 printf "\n\n"
 
 # sddi-social #################################################################
@@ -46,17 +48,19 @@ cd ../sddi-social
 
 # sddi-social:TAG
 docker build . \
-  --no-cache \
   --build-arg "BASEIMAGE_REPOSITORY=$IMAGE_REPOSITORY/ckan-sddi" \
   --build-arg BASEIMAGE_VERSION=${IMAGE_TAG} \
   -t "$IMAGE_REPOSITORY/ckan-sddi-social:${IMAGE_TAG}"
 
+docker push "$IMAGE_REPOSITORY/ckan-sddi-social:${IMAGE_TAG}"
+
 # sddi-social:TAG-debug
 docker build . \
-  --no-cache \
   --build-arg "BASEIMAGE_REPOSITORY=$IMAGE_REPOSITORY/ckan-sddi" \
   --build-arg BASEIMAGE_VERSION=${IMAGE_TAG}-debug \
   -t "$IMAGE_REPOSITORY/ckan-sddi-social:${IMAGE_TAG}-debug"
+
+docker push "$IMAGE_REPOSITORY/ckan-sddi-social:${IMAGE_TAG}-debug"
 
 cd ..
 printf "\n\n"
