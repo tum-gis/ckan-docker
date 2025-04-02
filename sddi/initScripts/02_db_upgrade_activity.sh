6e16d2730b6370f890718fbf5831b16114997cf8
Marija <93824048+MarijaKnezevic@users.noreply.github.com>
2025-04-02 17:43:52 +0200

    Add files via upload

---
+++ sddi/initScripts/02_db_upgrade_activity.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+
+echo "Upgrading database for activity plugin ..."
+ckan -c "${CKAN_INI}" db upgrade -p activity || true
+echo "Upgrading database for activity plugin ...done!"
